6e68bc7bbcfb288b5ce1abda085034e4381d0f5c
emerge <i.i.babko@gmail.com>
2016-06-30 00:07:12 +0300

    change reviews section styles

--- resources/views/sections/reviews.blade.php
+++ resources/views/sections/reviews.blade.php
@@ -1,15 +1,19 @@
 <section class="reviews">
     <div class="container">
-        <h2 style="font-size: 48px">Отзывы</h2>
-        <div class="reviews-block">
-            <div class="review">
-                First review   
-            </div>
-            <div class="review">
-                second review   
-            </div>
-            <div class="review">
-                third review   
+        <h2 style="font-size: 48px; color: #5d5d5d">Отзывы</h2>
+        <div class="reviews-block-wrapper">
+            <i class="fa fa-quote-left" aria-hidden="true"></i>
+            <i class="fa fa-quote-right" aria-hidden="true"></i>
+            <div class="reviews-block">
+                <div class="review">
+                    This is small description about what this product is all about and why this is the BEST choice for YOU. And here goes some random text, complete insane. Last sentence is gonna be this one
+                </div>
+                <div class="review">
+                    second review   
+                </div>
+                <div class="review">
+                    third review   
+                </div>
             </div>
         </div>
         <button class="add-review-button">Добавить</button>

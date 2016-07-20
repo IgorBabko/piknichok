62c3510f6c4fdadded525270817b7c2f7f548f52
Igor Babko <i.i.babko@gmail.com>
2016-07-20 09:06:46 +0300

    WIP

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
     public function index()
     {
         return view('index');
+    }
+
+    public function order(Request $request)
+    {
+        $this->validate([
+            'email' => 'email'
+        ]);
+
+        Order::create($request->only('name', 'email'));
+    }
+
+    public function email(Request $request)
+    {
+        Mail::send('emails.webinar', [], function ($m) use ($request) {
+            $m->from();
+            $m->to(env('MAIL_TO', 'i.i.babko@gmail.com'))->subject('Piknichok');
+        });
     }
 }
